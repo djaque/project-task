d52fea74ff80e54d92551ac44f3bc6664236b201
Dany Jaque <djaque@gmail.com>
2023-07-26 16:43:38 -0400

    fix/front - Fix files

---
+++ frontend/task-web/src/components/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header-main">
+        <p>
+        <img src="./logo192.png" alt="logo" width={150} height={30}/>
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Header;
